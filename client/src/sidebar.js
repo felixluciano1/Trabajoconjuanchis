6a6717c041ce3f1ea9a208099cd264310e211fc1
felixequide <prodigio14444@gmail.com>
2025-11-04 17:31:09 -0500

    Adaptar sidebar para app.js(modelo)

--- client/src/sidebar.js
+++ client/src/sidebar.js
@@ -2,25 +2,55 @@ import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
-function Sidebar() {
+function Sidebar({ usuarioLogueado, setContenido }) {
+  
+  const manejarClickCalculo = () => {
+    setContenido("calculo");
+  };
+
+  const manejarClickRegistro = () => {
+    setContenido("registro");
+  };
+
+  const manejarLogin = () => {
+    setContenido("login");
+  };
+
   return (
     <div className="sidebar bg-dark text-light p-4">
       <h4 className="text-center mb-4 text-warning">INMOBILIARIA</h4>
       <ul className="nav flex-column text-end">
         <li className="nav-item mb-3">
-          <a href="#inicio" className="nav-link text-light">Inicio</a>
+          <a href="#inicio" className="nav-link text-light" onClick={() => setContenido("inicio")}>Inicio</a>
+        </li>
+
+        {/* Mostrar login si NO está logueado */}
+        {!usuarioLogueado && (
+          <li className="nav-item mb-3">
+            <a href="#login" className="nav-link text-light" onClick={manejarLogin}>Iniciar Sesión</a>
+          </li>
+        )}
+
+        {/* Mostrar opciones solo si está logueado */}
+        {usuarioLogueado && (
+          <>
+            <li className="nav-item mb-3">
+              <a href="#calculo" className="nav-link text-light" onClick={manejarClickCalculo}>Calcular propiedad</a>
+            </li>
+            <li className="nav-item mb-3">
+              <a href="#registro" className="nav-link text-light" onClick={manejarClickRegistro}>Registrar propiedad</a>
+            </li>
+          </>
+        )}
+
+        <li className="nav-item mb-3">
+          <a href="#oficinas" className="nav-link text-light" onClick={() => setContenido("oficinas")}>Oficinas</a>
         </li>
         <li className="nav-item mb-3">
-          <a href="#buscar" className="nav-link text-light">Iniciar Sesion</a>
+          <a href="#asesores" className="nav-link text-light" onClick={() => setContenido("asesores")}>Asesores</a>
         </li>
         <li className="nav-item mb-3">
-          <a href="#oficinas" className="nav-link text-light">Oficinas</a>
-        </li>
-        <li className="nav-item mb-3">
-          <a href="#asesores" className="nav-link text-light">Asesores</a>
-        </li>
-        <li className="nav-item mb-3">
-          <a href="#contactanos" className="nav-link text-light">Contáctanos</a>
+          <a href="#contactanos" className="nav-link text-light" onClick={() => setContenido("contactanos")}>Contáctanos</a>
         </li>
       </ul>
     </div>
